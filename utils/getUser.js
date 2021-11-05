a54c919256c11e7daf77a1ec399dee5e3965ea57
AxuIsCool <321axu123@gmail.com>
2021-11-05 15:44:57 +0100

    cached user in local storage

--- utils/getUser.js
+++ utils/getUser.js
@@ -1,9 +1,24 @@
-export async function getUser() {
-  try {
-    const res = await fetch(`http://localhost:3000/api/user`);
-    const { user } = await res.json();
+import { isServer } from "./isServer";
+import { localStorageKeys } from "../constants";
 
-    return user;
+const userKey = localStorageKeys.user;
+
+export async function getUser() {
+  if (isServer()) return;
+
+  try {
+    let _user = localStorage.getItem(userKey);
+
+    if (!_user || _user === "null" || _user === "") {
+      const res = await fetch(`http://localhost:3000/api/user`);
+      const { user } = await res.json();
+
+      _user = JSON.stringify(user);
+      // cache user in local storage
+      localStorage.setItem(userKey, _user);
+    }
+
+    return JSON.parse(_user);
   } catch {
     return null;
   }
